1f0d1da5b98d7bab6b2f777e3c8633f013b8173e
Vladimir Radojkovic <vladimirradojkovic@air.local>
2025-05-11 15:59:23 +0200

    added mysql scripts for creating DB

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
-    echo "hello world"
-?>
+session_start();
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+switch ($page) {
+    case 'login':
+        include 'views/auth/login.php';
+        break;
+    case 'register':
+        include 'views/auth/register.php';
+        break;
+    case 'admin_products':
+        include 'views/admin/product_list.php';
+        break;
+    case 'add_product':
+        include 'views/admin/add_product.php';
+        break;
+    case 'cart':
+        include 'views/user/cart.php';
+        break;
+    case 'catalog':
+        include 'views/user/product_catalog.php';
+        break;
+    case 'checkout':
+        include 'views/user/checkout.php';
+        break;
+    case 'logout':
+        include 'controllers/AuthController.php';
+        // logout();  
+        break;
+    default:
+        include 'views/user/product_catalog.php';
+}
